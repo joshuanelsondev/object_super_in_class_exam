98eab6e88094d7b1febffff5dba350a88176a3f6
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-07 22:26:07 -0400

    complete tests

--- src/main/java/org/example/DiscountedProduct.java
+++ src/main/java/org/example/DiscountedProduct.java
@@ -1,5 +1,17 @@
 package org.example;
 
 public class DiscountedProduct extends Product {
+    public double discountPercentage;
+
+    public DiscountedProduct(String name, double price, String description, double discountPercentage) {
+        super(name, price, description);
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double calculateDiscountedPrice() {
+
+       return price * ((100 - discountPercentage) * 0.01);
+    }
+
 
 }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,5 +1,31 @@
 package org.example;
 
 public class Product {
+    private String name;
+    public double price;
+    private String description;
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void displayProductInfo() {
+        System.out.printf("Product Name: %s, Product Price: %s, Product Description: %s", this.name, this.price, this.description);
+    }
+
 
 }
